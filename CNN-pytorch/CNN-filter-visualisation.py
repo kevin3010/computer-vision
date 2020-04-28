04f9329dfdebe0aeeaa6ddfeea25b1789bcff8fe
kevin3010 <42439376+kevin3010@users.noreply.github.com>
2020-04-28 13:18:09 +0530

    Update CNN-filter-visualisation.py

--- CNN-pytorch/CNN-filter-visualisation.py
+++ CNN-pytorch/CNN-filter-visualisation.py
@@ -23,11 +23,15 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1,4,kernel_size = (k_height,k_width),bias=False)
         self.conv1.weights = torch.nn.Parameter(weight)
         
+        self.pool = nn.MaxPool2d(4,4)
+        
     def forward(self,x):
         convx = self.conv1(x)
         activated_convx = F.relu(convx)
         
-        return convx,activated_convx
+        pooledx = self.pool(activated_convx)
+        
+        return convx,activated_convx,pooledx
         
 weight = torch.from_numpy(filters).unsqueeze(1).type(torch.FloatTensor)
 
@@ -42,7 +46,7 @@ gray_img = gray_img.astype('float32')/255
 img_tensor = torch.from_numpy(gray_img).unsqueeze(0).unsqueeze(1)
 
 
-conv,conv_active = model(img_tensor)
+conv,conv_active,pooled = model(img_tensor)
 
 fig,axs1 = plt.subplots(1,4,figsize=(20,20))
 axs1 = axs1.flatten()
@@ -51,6 +55,8 @@ for i,ax in enumerate(axs1):
     
     ax.imshow(conv[0][i].data.numpy(),cmap='gray')
     
+    
+    
 fig,axs2 = plt.subplots(1,4,figsize=(20,20))
 axs2 = axs2.flatten()    
     
@@ -58,6 +64,9 @@ for i,ax in enumerate(axs2):
     ax.imshow(conv_active[0][i].data.numpy(),cmap='gray')
     
     
-
-
-
+    
+fig,axs3 = plt.subplots(1,4,figsize=(20,20))
+axs3 = axs3.flatten()    
+    
+for i,ax in enumerate(axs3):
+    ax.imshow(pooled[0][i].data.numpy(),cmap='gray')
